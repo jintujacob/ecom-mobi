b2cefb429b68b299656182bde57a61b0a5695be6
jintujacob <jintu.jacob@gmail.com>
2013-03-16 21:46:31 +0530

    seller profile  form submission

---
+++ dbfunctions/profilemanagementfunction.php
@@ -0,0 +1,43 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+include("connection.php");
+
+
+function doAddProfileDB($data  ){
+
+$query ="insert into sellerprofile ".
+			"(	display_name  ,
+				operator_name ,
+				email         ,
+				address       ,
+				zipcode       ,
+				ph_no         ,
+				landmark      ,
+				near_city     ,
+				latitude      ,
+				longitude 
+ 			 )values ( '".
+				
+				$data[0] 	."', '".
+				$data[1] 	."', '".
+				$data[2] 	."', '".
+				$data[3] 	."', '".
+				$data[4] 	."', '".
+				$data[5] 	."', '".
+				$data[6] 	."', '".
+				$data[7] 	."', '".
+				$data[8] 	."', '".
+				$data[9] 	."') " ;
+					
+	$result = mysql_query($query);		 	
+}
+
+
+
+
+
+
+
+?>
---
+++ services/sellerprofileaction.php
@@ -0,0 +1,36 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+include('../dbfunctions/profilemanagementfunction.php');
+
+$action 	= (isset( $_POST['action'])) 	? $_POST['action'] 	: "" ;
+
+if($action == "savemyprofile")
+{
+	$displayname  = (isset( $_POST["displayname"] ))  ? $_POST["displayname"] 	: "" ;	
+	$operatorname =(isset( $_POST["operatorname"]))   ? $_POST["operatorname"]: "" ;
+	$email 	      = (isset( $_POST["email"]))         ? $_POST["email"]: "" ;
+	$address      = (isset($_POST["address"]))        ? $_POST["address"]: "" ;
+	$zipcode      = (isset($_POST["zipcode"]))        ? $_POST["zipcode"]: "" ;
+	$contactno	  = (isset($_POST["contactno"])) 	  ? $_POST["contactno"]	: "" ;
+	$landmark	  = (isset($_POST["landmark"])) 	  ? $_POST["landmark"]	: "" ;
+	$nearcity	  = (isset($_POST["nearcity"])) 	  ? $_POST["nearcity"]	: "" ;
+	$latitude	  = (isset($_POST["latitude"])) 	  ? $_POST["latitude"]	: "" ;
+	$longitude	  = (isset($_POST["longitude"])) 	  ? $_POST["longitude"]  : "" ;
+	$data = array( $displayname, $operatorname, $email, $address, $zipcode, $contactno, $landmark, $nearcity, $latitude, $longitude);
+
+	doAddProfile($data);
+	
+		
+}
+
+
+ function doAddProfile($data)
+ {
+     doAddProfileDB($data);
+     echo json_encode(array("result"=>TRUE));
+	
+	 
+ }
+ 
